b3047666487233f523867623f83b648cb326604c
dikshyant <dikshyant.kashajoo@gmail.com>
2024-03-20 17:34:11 +0545

    expirement file started

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
+Commands used
 creating an environment 
 $ conda create -p env python=3.8 -y
 activating a virtual environment
 $ source activate ./env
+installing the requirements 
+pip install -r requirements.txt
+
+
+File Structure 
+setup.py = # it is the local package in virtual environment
--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 # local package in virtual environment
+from setuptools import find_packages,setup
+
+setup(
+    name = 'mcqgenrator',
+    version='0.0.1',
+    author = 'Dikshyant',
+    author_email='dikshyant.kashajoo@gmail.com',
+    install_requies=['openai','langchain','streamlit','python-dotenv','PyPDF2'],
+    packages=find_packages()
+)
